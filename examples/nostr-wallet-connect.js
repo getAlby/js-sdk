a0b6d3ba6e07ccba73e2a7ee946401db03a19ea0
Michael Bumann <hello@michaelbumann.com>
2023-03-28 00:37:44 +0200

    Add nostr wallet connect example

---
+++ examples/nostr-wallet-connect.js
@@ -0,0 +1,22 @@
+import * as crypto from 'node:crypto'; // required in node.js
+global.crypto = crypto; // required in node.js
+import 'websocket-polyfill' // required in node.js
+
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+import { webln as providers } from "../dist/index.module.js";
+
+const rl = readline.createInterface({ input, output });
+
+const nwcUrl = await rl.question('Nostr Wallet Connect URL (nostrwalletconnect://...): ');
+const invoice = await rl.question('Lightning invoice: ');
+rl.close();
+
+const webln = new providers.NostrWebLNProvider({ nostrWalletConnectUrl: nwcUrl });
+await webln.enable();
+const response = await webln.sendPayment(invoice);
+
+console.log(response);
+
+webln.close();
