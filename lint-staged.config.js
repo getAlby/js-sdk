8ad9df84394a12d88153037d97130de1bdbad22d
pavanjoshi914 <pavanj914@gmail.com>
2023-08-10 11:39:36 +0530

    feat: add prettier create rules for prettier according to the files in codebase add husky pre commit hook signed-off-by: pavan joshi <pavanj914@gmail.com>

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+export default {
+    "src/**/*.{js,ts,jsx}": [
+      "prettier --write",
+    ],
+    "examples/**/*.{js,mjs}": [
+      "prettier --write",
+    ],
+  
+    "package.json": ["prettier --write"],
+  };
+  
