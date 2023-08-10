07e0ffc63c044d65895785ff01962e639b92f122
pavanjoshi914 <pavanj914@gmail.com>
2023-08-10 15:44:28 +0530

    feat: add eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,13 @@
 export default {
     "src/**/*.{js,ts}": [
-      "prettier --write",
+      "eslint --fix --max-warnings 0",
+      "prettier --write"
     ],
     "examples/**/*.{js,mjs,jsx}": [
-      "prettier --write",
+      "prettier --write"
     ],
   
     "package.json": ["prettier --write"],
+    "src/**/*.ts": () => "tsc --noEmit",
   };
   
