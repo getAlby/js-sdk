556cb2d5aff3ed2f5d715effcd731b82b2e051c8
Roland Bewick <roland.bewick@gmail.com>
2023-08-18 16:59:44 +0700

    fix: format examples

--- examples/nwc/get-balance.js
+++ examples/nwc/get-balance.js
@@ -1,18 +1,22 @@
-import * as crypto from 'node:crypto'; // required in node.js
+import * as crypto from "node:crypto"; // required in node.js
 global.crypto = crypto; // required in node.js
-import 'websocket-polyfill' // required in node.js
+import "websocket-polyfill"; // required in node.js
 
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
 import { webln as providers } from "../../dist/index.module.js";
 
 const rl = readline.createInterface({ input, output });
 
-const nwcUrl = await rl.question('Nostr Wallet Connect URL (nostrwalletconnect://...): ');
+const nwcUrl = await rl.question(
+  "Nostr Wallet Connect URL (nostrwalletconnect://...): ",
+);
 rl.close();
 
-const webln = new providers.NostrWebLNProvider({ nostrWalletConnectUrl: nwcUrl });
+const webln = new providers.NostrWebLNProvider({
+  nostrWalletConnectUrl: nwcUrl,
+});
 await webln.enable();
 const response = await webln.getBalance();
 
