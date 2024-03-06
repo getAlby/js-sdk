116116226be5d79b0ee6e953956caf87ad8c65b0
Roland Bewick <roland.bewick@gmail.com>
2024-03-06 12:00:49 +0700

    chore: add extra Nip47TimeoutError class

--- src/NWCClient.ts
+++ src/NWCClient.ts
@@ -146,8 +146,10 @@ export class Nip47Error extends Error {
  * A NIP-47 response was received, but with an error code (see https://github.com/nostr-protocol/nips/blob/master/47.md#error-codes)
  */
 export class Nip47WalletError extends Nip47Error {}
-export class Nip47PublishTimeoutError extends Nip47Error {}
-export class Nip47ReplyTimeoutError extends Nip47Error {}
+
+export class Nip47TimeoutError extends Nip47Error {}
+export class Nip47PublishTimeoutError extends Nip47TimeoutError {}
+export class Nip47ReplyTimeoutError extends Nip47TimeoutError {}
 export class Nip47PublishError extends Nip47Error {}
 export class Nip47ResponseDecodingError extends Nip47Error {}
 export class Nip47ResponseValidationError extends Nip47Error {}
