3592ae944d0e1302a7ca493faf5f0b739fc198a6
Roland Bewick <roland.bewick@gmail.com>
2024-07-09 20:03:36 +0700

    fix: update nwc client keysend examples to pass TLV values as hex

--- examples/nwc/client/multi-pay-keysend.js
+++ examples/nwc/client/multi-pay-keysend.js
@@ -17,6 +17,26 @@ const client = new nwc.NWCClient({
   nostrWalletConnectUrl: nwcUrl,
 });
 
+// Data from https://podcastindex.org/podcast/920666
+const boost = {
+  action: "boost",
+  value_msat: 1000,
+  value_msat_total: 1000,
+  app_name: "⚡ WebLN Demo",
+  app_version: "1.0",
+  feedID: "https://feeds.podcastindex.org/pc20.xml",
+  podcast: "Podcasting 2.0",
+  episode: "Episode 104: A New Dump",
+  ts: 21,
+  name: "⚡ WebLN Demo",
+  sender_name: "Sathoshi Nakamoto",
+  message: "Go podcasting!",
+};
+
+// from https://stackoverflow.com/a/50868276
+const toHexString = (bytes) =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
+
 const keysends = [
   {
     pubkey:
@@ -25,11 +45,11 @@ const keysends = [
     tlv_records: [
       {
         type: 696969,
-        value: "017rsl75kNnSke4mMHYE", // hello@getalby.com
+        value: toHexString(new TextEncoder().encode("017rsl75kNnSke4mMHYE")), // hello@getalby.com
       },
       {
-        type: 34349334,
-        value: "first keysend message",
+        type: 7629169,
+        value: toHexString(new TextEncoder().encode(JSON.stringify(boost))),
       },
     ],
   },
@@ -40,11 +60,11 @@ const keysends = [
     tlv_records: [
       {
         type: 696969,
-        value: "1KOZHzhLs2U7JIx3BmEY", // another Alby account
+        value: toHexString(new TextEncoder().encode("1KOZHzhLs2U7JIx3BmEY")), // another Alby account
       },
       {
-        type: 34349334,
-        value: "second keysend message",
+        type: 7629169,
+        value: toHexString(new TextEncoder().encode(JSON.stringify(boost))),
       },
     ],
   },
