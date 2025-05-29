6a604560cbfd2862eced157db70ba4c5c97a75d7
Dunsin <ayodejiabisuwa23@gmail.com>
2025-05-29 19:11:07 +0100

    feat(examples/lnclient) : change rewrite paywall's content to typescript

--- examples/lnclient/paywall.ts
+++ examples/lnclient/paywall.ts
@@ -4,39 +4,50 @@ import qrcode from "qrcode-terminal";
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
-import { LN, USD } from "../../dist/index.module.js";
+import { LN, USD } from "@getalby/sdk";
 
 const rl = readline.createInterface({ input, output });
 
-const nwcUrl =
-  process.env.NWC_URL ||
-  (await rl.question("Nostr Wallet Connect URL (nostr+walletconnect://...): "));
-rl.close();
+async function main(): Promise<void> {
+  const nwcUrl =
+    process.env.NWC_URL ||
+    (await rl.question(
+      "Nostr Wallet Connect URL (nostr+walletconnect://...): ",
+    ));
+  rl.close();
 
-const client = new LN(nwcUrl);
+  const client = new LN(nwcUrl);
 
-// request a lightning invoice that we show the user to pay
-const request = await client.requestPayment(USD(1.0), {
-  description: "best content",
-});
-
-qrcode.generate(request.invoice.paymentRequest, { small: true });
-console.info(request.invoice.paymentRequest);
-console.info("Please pay the above invoice within 60 seconds.");
-console.info("Waiting for payment...");
-
-// once the invoice got paid by the user run this callback
-request
-  .onPaid(() => {
-    console.info("received payment!");
-    client.close(); // when done and no longer needed close the wallet connection
-  })
-  .onTimeout(60, () => {
-    console.info("didn't receive payment in time.");
-    client.close(); // when done and no longer needed close the wallet connection
+  // request a lightning invoice that we show the user to pay
+  const request = await client.requestPayment(USD(1.0), {
+    description: "best content",
   });
 
-process.on("SIGINT", function () {
-  console.info("Caught interrupt signal");
-  process.exit();
+  qrcode.generate(request.invoice.paymentRequest, { small: true });
+  console.info(request.invoice.paymentRequest);
+  console.info("Please pay the above invoice within 60 seconds.");
+  console.info("Waiting for payment...");
+
+  // once the invoice got paid by the user run this callback
+  request
+    .onPaid(() => {
+      console.info("received payment!");
+      client.close(); // when done and no longer needed close the wallet connection
+      process.exit();
+    })
+    .onTimeout(60, () => {
+      console.info("didn't receive payment in time.");
+      client.close(); // when done and no longer needed close the wallet connection
+      process.exit();
+    });
+
+  process.on("SIGINT", function () {
+    console.info("Caught interrupt signal");
+    client.close();
+    process.exit();
+  });
+}
+
+main().catch((err) => {
+  console.error("Unexpected error:", err);
 });
