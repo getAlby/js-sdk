dd45954ef396fb8b041b334febf6465f03b36dc2
Dunsin <ayodejiabisuwa23@gmail.com>
2025-09-01 11:46:44 +0100

    rewrite in ts

--- examples/oauth/keysends.ts
+++ examples/oauth/keysends.ts
@@ -1,15 +1,18 @@
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
-
-import { oauth } from "../../dist/index.module.js";
-const { auth, Client } = oauth;
+import {  Client, OAuth2User } from "@getalby/sdk/oauth";
 
 const rl = readline.createInterface({ input, output });
 
-const authClient = new auth.OAuth2User({
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+  throw new Error("Please set CLIENT_ID and CLIENT_SECRET");
+}
+
+const authClient = new OAuth2User({
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
   callback: "http://localhost:8080/callback",
+  user_agent:"AlbySDK-Example/0.1 (keysends-demo)",
   scopes: [
     "invoices:read",
     "account:read",
