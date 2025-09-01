02db87a13234aa7fac8f53f43808b98b55dac227
Dunsin <ayodejiabisuwa23@gmail.com>
2025-09-01 10:15:29 +0100

    rewrite in ts

--- examples/oauth/invoices.ts
+++ examples/oauth/invoices.ts
@@ -1,16 +1,21 @@
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
-import { oauth } from "../../dist/index.module.js";
-const { auth, Client } = oauth;
+import {  Client, OAuth2User } from "@getalby/sdk/oauth";
 
 const rl = readline.createInterface({ input, output });
 
-const authClient = new auth.OAuth2User({
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+  throw new Error("Please set CLIENT_ID and CLIENT_SECRET");
+}
+
+
+const authClient = new OAuth2User({
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
   callback: "http://localhost:8080",
   scopes: ["invoices:read", "account:read", "balance:read"],
+  user_agent:"AlbySDK-Example/0.1 (invoices-demo)",
   token: {
     access_token: undefined,
     refresh_token: undefined,
