27fd4d9fe0ea59d4473634db826e5ca4906429f9
Dunsin <ayodejiabisuwa23@gmail.com>
2025-10-01 13:31:11 +0100

    feat: use either accessToken or oauth2 in oauth exmaples

--- examples/oauth/send-to-ln-address.ts
+++ examples/oauth/send-to-ln-address.ts
@@ -1,39 +1,9 @@
-import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
-import { Client, OAuth2User } from "@getalby/sdk/oauth";
-
 import { LightningAddress } from "@getalby/lightning-tools";
+import { getAuthClient } from "./helper";
 
-const rl = readline.createInterface({ input, output });
+const userAgent = "AlbySDK-Example/0.1 (send_to_ln_address-demo)"
 
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-  throw new Error("Please set CLIENT_ID and CLIENT_SECRET");
-}
-
-const authClient = new OAuth2User({
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  callback: "http://localhost:8080",
-  user_agent:"AlbySDK-Example/0.1 (send_to_ln_address-demo)",
-  scopes: ["payments:send"],
-  token: {
-    access_token: undefined,
-    refresh_token: undefined,
-    expires_at: undefined,
-  }, // initialize with existing token
-});
-
-console.log(`Open the following URL and authenticate the app:`);
-console.log(await authClient.generateAuthURL());
-console.log("----\n");
-
-const code = await rl.question("Code: (localhost:8080?code=[THIS CODE]: ");
-rl.close();
-
-await authClient.requestAccessToken(code);
-console.log(authClient.token);
-const client = new Client(authClient);
-
+const client = await getAuthClient(userAgent);
 const ln = new LightningAddress("hello@getalby.com");
 // fetch the LNURL data
 await ln.fetch();
