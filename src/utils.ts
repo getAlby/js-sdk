96bcdbf93fd51a109166e73a01a3d420241585a1
naveenkumar29052006 <nn03092005@gmail.com>
2026-01-23 22:17:41 +0530

    Added an explicit return

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,17 @@
+import crypto from "crypto";
 // from https://stackoverflow.com/a/50868276
 const toHexString = (bytes: Uint8Array) =>
   bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
 
-async function generatePreimageAndPaymentHash() {
-  const preimageBytes = crypto.getRandomValues(new Uint8Array(32));
+async function generatePreimageAndPaymentHash(): Promise<{
+  preimage: string;
+  paymentHash: string;
+}> {
+  const preimageBytes = crypto.randomBytes(32);
   const preimage = toHexString(preimageBytes);
 
-  const hashBuffer = await crypto.subtle.digest("SHA-256", preimageBytes);
-  const paymentHash = toHexString(new Uint8Array(hashBuffer));
+  const hashBuffer = crypto.createHash("sha256").update(preimageBytes).digest();
+  const paymentHash = toHexString(hashBuffer);
 
   return { preimage, paymentHash };
 }
