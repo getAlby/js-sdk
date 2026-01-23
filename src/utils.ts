b688bc409eddbec734421bab2705302c74d02398
naveenkumar29052006 <nn03092005@gmail.com>
2026-01-23 22:03:50 +0530

    feat(utils): add preimage and payment hash helper

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,19 @@
 // from https://stackoverflow.com/a/50868276
-export const toHexString = (bytes: Uint8Array) =>
+const toHexString = (bytes: Uint8Array) =>
   bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
+
+async function generatePreimageAndPaymentHash() {
+  const preimageBytes = crypto.getRandomValues(new Uint8Array(32));
+  const preimage = toHexString(preimageBytes);
+
+  const hashBuffer = await crypto.subtle.digest("SHA-256", preimageBytes);
+  const paymentHash = toHexString(new Uint8Array(hashBuffer));
+
+  return { preimage, paymentHash };
+}
+
+
+export {
+  toHexString,
+  generatePreimageAndPaymentHash,
+};
