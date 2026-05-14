848bdb26befc17e10267aaab6353d4589716b0f3
Roland Bewick <roland.bewick@gmail.com>
2026-05-14 21:53:31 +0700

    fix: test config for esm

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      { tsconfig: "tsconfig.test.json", useESM: true },
+    ],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
 };
--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -1,11 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/e2e/**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/e2e/browser'],
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  testMatch: ["<rootDir>/e2e/**/*.test.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/browser"],
   maxWorkers: 1, // Run e2e tests serially to avoid faucet rate limiting
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.e2e.json' }],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      { tsconfig: "tsconfig.e2e.json", useESM: true },
+    ],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 };
